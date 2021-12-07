00e215ba01c9c700d81b10d58cf60bc5645c20ab
João Bruno <jnnlbruno@gmail.com>
2021-12-07 20:31:14 +0000

    Added day7

---
+++ day7.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+positions = File.read(ARGV[0]).split(',').each_with_object(Hash.new(0)) { |f, h| h[f.to_i] += 1 }
+
+positions_range = (positions.keys.min..positions.keys.max)
+
+p positions_range.flat_map { |ix| positions.map { |x, t| (x - ix).abs * t }.sum }.min
+
+p positions_range.flat_map { |ix| positions.map { |x, t| (0..(x - ix).abs).sum * t }.sum }.min
