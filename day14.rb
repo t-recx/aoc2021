9e04ba15edfd2ab20e90702879ec07b83290dcf9
João Bruno <jnnlbruno@gmail.com>
2021-12-15 20:22:17 +0000

    Reworked solution to use less memory

--- day14.rb
+++ day14.rb
@@ -1,94 +1,41 @@
 #!/usr/bin/env ruby
 
-class Node
-    attr_accessor :value
-    attr_accessor :prev
-    attr_accessor :nxt
+def process template, rules, steps
+  h = Hash.new(0)
 
-    def initialize prev = nil, nxt = nil
-      @prev = prev
-      @nxt = nxt
-    end
+  template.each_cons(2) do |a, b|
+    h[a + b] += 1
+  end
 
-    def link value
-      new_node = Node.new 
-      new_node.value = value
+  steps.times do |step|
 
-      if @nxt
-        new_node.nxt = @nxt
-        @prev = new_node
+    nh = Hash.new(0)
+
+    h.each do |key, value|
+      if rules[key] 
+        nh[key[0] + rules[key]] += h[key]
+        nh[rules[key] + key[1]] += h[key]
+
       end
-
-      @nxt = new_node
     end
-end
-
-def create_list template
-  start = Node.new
-
-  node = start
-  
-  template.each do |c|
-    node.value = c
-    nxt = Node.new node
-    node.nxt = nxt
-    node = nxt
+    
+    h = nh
   end
 
-  node.nxt = nil
-
-  start
-end
-
-def print_list node
-  loop do 
-    print node.value
-    node = node.nxt  
-    break if node.nil?
-  end
-
-  puts
-end
-
-def get_counts_by_value node
   counts = Hash.new(0)
 
-  loop do 
-    counts[node.value] += 1 
-    node = node.nxt  
-    break if node.nil? || node.value.nil?
-  end
+  h.map { |k, v| [k[0], v] }.each { |k, v| counts[k] += v }
 
-  counts
+  counts[template.last] += 1
+
+  return counts.values.max - counts.values.min
 end
 
 input = File.readlines(ARGV[0]).map(&:strip)
 
 template = input.first.chars
 
-rules = input.drop(2).map { |line| line.split(' -> ').map(&:chars).flatten}
+rules = input.drop(2).map { |line| line.split(' -> ') }.each_with_object({}) { |rule, hash| hash[rule[0]] = rule[1] }
 
-start_a = create_list(template)
-
-node = start_a
-
-10.times do |step|
-  loop do 
-    rules.each do |a, b, r|
-      if node.value == a && node.nxt && node.nxt.value == b
-        node.link(r)
-        node = node.nxt
-        break
-      end
-    end
-
-    node = node.nxt
-    break if node == nil || node.nxt == nil
-  end
-
-  node = start_a
-end
-
-counts = get_counts_by_value(node).values
-
-p counts.max - counts.min
+p process(template, rules, 10)
+p process(template, rules, 40)
